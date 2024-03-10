44838163caa2a08f0965103750360ba346d3d8d7
OleksandrMaimeskul <maymeskulsasha@gmail.com>
2024-03-10 16:01:59 +0200

    Lesson05Homework10 2nd Batch (Retake)

--- src/Lesson05Homework10.java
+++ src/Lesson05Homework10.java
@@ -1,13 +1,15 @@
 public class Lesson05Homework10 {
     public static void main(String[] args) {
         System.out.println("There are numbers of shuttles proceeding to Mars: ");
-        for (int shuttle = 1; shuttle <= 100; shuttle++) {
-            if (shuttle % 10 != 4 && shuttle % 10 != 9 && shuttle / 10 != 4 && shuttle / 10 != 9) {
-                System.out.println(shuttle);
-            } else {
-                System.out.println("--");
+        int shuttleNumber = 1;
+        for (int shuttle = 1; shuttleNumber <= 100; shuttle++) {
+            if (shuttle % 10 != 4 && shuttle % 10 != 9 && shuttle / 10 != 4 && shuttle / 10 != 9
+                    && shuttle % 100 != 40 && shuttle % 100 != 41 && shuttle % 100 != 42 && shuttle % 100 != 43
+                    && shuttle % 100 != 44 && shuttle % 100 != 45 && shuttle % 100 != 46 && shuttle % 100 != 47
+                    && shuttle % 100 != 48 && shuttle % 100 != 49) {
+                System.out.println(shuttleNumber + "." + "Shuttle # " + shuttle);
+                shuttleNumber++;
             }
         }
-        System.out.println("Indication -- is missing numbers because contain 4 or 9.");
     }
 }
